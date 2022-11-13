804cd6561414f05095fed85783926d4527d6bdc1
oniel santiago <onielst84@gmail.com>
2022-11-13 14:58:35 -0300

    validando dados do formulário

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
+const moedaSelect = document.querySelector('#moeda');
 const criptomoedasSelect = document.querySelector('#criptomonedas');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+  moeda: "",
+  criptomoeda: ""
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+
   consultarCriptomoedas();
+  
+  formulario.addEventListener('submit', submitFormulario);
+
+  moedaSelect.addEventListener('change', lerValor);
+  criptomoedasSelect.addEventListener('change', lerValor);
+
 });
 
 function consultarCriptomoedas(){
@@ -13,6 +27,7 @@ function consultarCriptomoedas(){
 }
 
 function selectCriptomoedas(criptomoedas){
+  console.log(criptomoedas)
   criptomoedas.forEach(cripto => {
     const { Name, FullName } = cripto.CoinInfo;
 
@@ -22,5 +37,43 @@ function selectCriptomoedas(criptomoedas){
 
     criptomoedasSelect.appendChild(option)
   });
+  
+}
+
+function lerValor(e){
+  objBusqueda[e.target.name] = e.target.value;
+  console.log(objBusqueda)
+}
+
+function submitFormulario(e){
+  e.preventDefault();
+
+  //validar o formulario
+  const { moeda, criptomoeda } = objBusqueda;
+
+  if(moeda === "" || criptomoeda === ""){
+    mostrarAlerta('Os dois campos são obrigatórios');
+    return;
+  }
+
+  //consultar a API com os resultados
+}
+
+function mostrarAlerta(mensagem){
+  
+  const existeErro = document.querySelector('.error');
+
+  if(!existeErro){
+    const divMensagem = document.createElement('p');
+    divMensagem.classList.add('error');
+    divMensagem.textContent = mensagem;
+
+    formulario.appendChild(divMensagem);
+
+    setTimeout(() => {
+      divMensagem.remove(); 
+    }, 2500);
+  }
+
   
 }
